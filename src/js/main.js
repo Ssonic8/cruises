905dd26a2a950cf897e94a376387ff1a7a656e61
Ssonic8 <gladunalexv@gmail.com>
2018-11-30 20:08:50 +0300

    popup added&js finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
 			$('.main-menu__link').click(function(){
+				event.preventDefault();
 				$('.main-menu__list').toggleClass('main-menu--active');
 			});
 
 			$('.catalog__card--abrau').click(function(event){
 				event.preventDefault();
 				$('.modal').removeClass('visually-hidden');
+				$('.overlay').show();
 				$('.slider-top').slick({
 				  slidesToShow: 1,
 				  slidesToScroll: 1,
 				  arrows: false,
 				  fade: true,
-				  asNavFor: '.slider-nav'
+				  asNavFor: '.slider-nav',
+				  responsive: [
+				    {
+				      breakpoint: 768,
+				      settings: {
+				        arrows: true,
+				        centerMode: true,
+				        slidesToShow: 1
+				      }
+				    }
+				  ]
 				});
 				$('.slider-nav').slick({
 				  slidesToShow: 4,
@@ -24,7 +36,9 @@ $(document).ready(function(){
 			});
 
 			$('.modal-close').click(function(){
+				event.preventDefault();
 				$('.modal').addClass('visually-hidden');
+				$('.overlay').hide();
 			});
 			
 		});
